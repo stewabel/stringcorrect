fe3c6af301ee277ddc14913c56ab6c776e2a06ab
Stew Abel <stew@stewartabel.co.uk>
2013-08-08 23:17:48 +0100

    learns two words and recalls

--- stringcorrect.js
+++ stringcorrect.js
@@ -1,9 +1,9 @@
-var correct = [];
+var correct = {};
 
 exports.correct = function(original, corrected){
-	correct = [corrected];
+	correct[original] = corrected;
 }
 
 exports.suggest = function(word){
-	return correct;
+	return [correct[word]];
 }
--- test/stringcalculatortest.js
+++ test/stringcalculatortest.js
@@ -13,6 +13,20 @@ describe ('StringCorrect', function (){
 
 			assert.equal(result[0], CORRECTED);
 		})
+
+		it('should learn two words', function(){
+			var ORIGINAL1 = "Original1";
+			var ORIGINAL2 = "Original2";
+			var CORRECTED1 = "Corrected1";
+			var CORRECTED2 = "Corrected2";
+			sc = Object.create(StringCorrect);
+			
+			sc.correct(ORIGINAL1, CORRECTED1);
+			sc.correct(ORIGINAL2, CORRECTED2);
+
+			assert.equal(sc.suggest(ORIGINAL1)[0], CORRECTED1);
+			assert.equal(sc.suggest(ORIGINAL2)[0], CORRECTED2);
+		})
 	})
 })
 
